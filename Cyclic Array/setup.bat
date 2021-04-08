28a4c985debdc5a4c2bcc627afeb5a14f194ac80
Sumudu Madushanka <ngsmadushanka.16@cse.mrt.ac.lk>
2021-04-08 19:00:12 +0530

    Cyclic Array

---
+++ Cyclic Array/setup.bat
@@ -0,0 +1,59 @@
+:: Cyclic Array compile file
+@echo OFF
+
+:: Compile the cpp files
+echo Start compile cpp files...
+mkdir temp_compile
+set /a modified=0
+if exist .\object_files\ (
+    echo object_files dir already created
+) else (
+    mkdir object_files
+    GOTO :Compile_cyclic_array
+)
+GOTO :Cyclic_Array
+
+:: Compile the "cyclic_array.cpp"
+:Cyclic_Array
+if exist .\object_files\cyclic_array.o (
+    GOTO :Has_cyclic_array_o
+) else (
+    GOTO :Compile_cyclic_array
+)
+
+:Has_cyclic_array_o
+copy .\cyclic_array.cpp .\temp_compile
+copy .\object_files\cyclic_array.o .\temp_compile
+cd .\temp_compile
+for /f %%i in ('dir /b /o:d cyclic_array.o cyclic_array.cpp') do set latest_file=%%i
+cd ..
+if "%latest_file%"=="cyclic_array.cpp" (
+    GOTO :Compile_cyclic_array
+) else (
+    echo cyclic_array.o already compiled
+    GOTO :Link_Obj_Files
+)
+
+:Compile_cyclic_array
+echo Compiling cyclic_array.cpp...
+g++ -c ./cyclic_array.cpp -o ./object_files/cyclic_array.o -std=c++11
+set /a modified=1
+echo Compiling cyclic_array.cpp Completed
+
+:: Link the object files
+:Link_Obj_Files
+if %modified%==1 (
+    GOTO :Linking_Obj
+) else (
+    echo Nothing was modified
+    GOTO :End_Compilation
+)
+:Linking_Obj
+echo Linking the object files...
+g++ ./object_files/*.o -o CyclicArrayDemo
+echo Completed the linking objects
+
+:End_Compilation
+echo Completed the compilation
+echo Cyclic Array compilation successfull
+rmdir /s /q .\temp_compile
